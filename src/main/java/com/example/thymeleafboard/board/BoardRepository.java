0814517d48330a4d04d0df9e4e8ea323a051686a
Universenn <dnwndhks98@gmail.com>
2023-09-13 16:09:13 +0900

    [feat] page 처리 및 연습

--- src/main/java/com/example/thymeleafboard/board/BoardRepository.java
+++ src/main/java/com/example/thymeleafboard/board/BoardRepository.java
@@ -1,6 +1,16 @@
 package com.example.thymeleafboard.board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAll(Pageable pageable);
+
+    Page<Board> findByTitle(Pageable pageable,String title);
+//    Page<Board> findByTitleLike(Pageable pageable,String title);
+//    Page<Board> getFirstByTitle(Pageable pageable,String title);
+    Page<Board> findFirstByTitle(Pageable pageable,String title);
+
+
 }
